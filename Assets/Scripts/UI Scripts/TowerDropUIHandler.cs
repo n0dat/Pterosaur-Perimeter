bb95c9d284e3468db3c69c5f4ff7aafc3340c03f
Dane Ferguson <98064151+InlaidData@users.noreply.github.com>
2023-10-11 17:41:27 -0600

    Added initial tower dropper UI design.
    UI for tower dropper is interactive. Will add Dinos later.

---
+++ Assets/Scripts/UI Scripts/TowerDropUIHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDropUIHandler : MonoBehaviour
+{
+    //These variables MUST be set in unity for the script to work.
+    [SerializeField] private GameObject towerContainer;
+    [SerializeField] private int slideDelta;
+    private bool isExpanded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Default when level loads will be expanded.
+        this.isExpanded = true;
+    }
+
+    //Called when the mouse hovers over expandHandle.
+    void OnMouseEnter()
+    {
+        Animator animator = towerContainer.GetComponent<Animator>();
+        if (this.isExpanded)
+        {
+            this.isExpanded = false;
+            animator.SetTrigger("HoveredWhileOpened");
+        }
+        else
+        {
+            this.isExpanded = true;
+            animator.SetTrigger("HoveredWhileClosed");
+        }
+    }
+    
+    //Called when the mouse leaves the 
+    void OnMouseExit()
+    {
+        
+    }
+}
---
+++ Assets/TowerDropExpandScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDropExpandScript : MonoBehaviour
+{
+     void OnMouseOver()
+     {
+          Debug.Log("Mouse is over GameObject.");
+     }
+}
