ae5e30a3b58b2b2c581c73013da0c81892ee1d3f
diegomorales30 <diegovmo96@gmail.com>
2023-10-15 17:17:19 -0600

    Added code for switching between hover and no hover

--- Assets/Scripts/Menus/PauseMenu/Resume.cs
+++ Assets/Scripts/Menus/PauseMenu/Resume.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Resume : MonoBehaviour
+
+public class Resume : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Image image;
+    public Sprite newSprite;
+    public Sprite oldSprite;
+
+    private bool isHover;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isHover = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {   
+            image.sprite = newSprite;
+        
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+            image.sprite = oldSprite;        
     }
 }
