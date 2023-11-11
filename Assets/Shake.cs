e99a52bc64286b48b77a6c3a992c2a90de43bc77
Joe Banuelos <joeaban@nmsu.edu>
2023-11-10 18:04:15 -0700

    Testing out Screen shake
    Done on a test screen, I'll need to fix the script

---
+++ Assets/Shake.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    /* Evey time I try to write a script it break the compiler
+    public Animator canAnim;
+
+    public void CamShake() {
+        canAnim, setTrigger("Shake");
+    }
+    */
+}
