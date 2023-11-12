15658d74931db02e72f565b42d1dd60cdf559de0
Dane Ferguson <98064151+InlaidData@users.noreply.github.com>
2023-11-12 15:59:28 -0700

    Push

---
+++ Assets/Scripts/Disaster/MeteorRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorRotate : MonoBehaviour
+{
+    private GameObject m_meteorSprite;
+
+    public void rotate()
+    {
+       
+    }
+    
+}
