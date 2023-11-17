e68f5ff69bfc1cf1a95874283ca4ac21f580a15f
Joe Banuelos <joeaban@nmsu.edu>
2023-11-17 12:21:18 -0700

    The intro sequence now plays from the initializer scene

--- Assets/Scripts/Menus/Initializer.cs
+++ Assets/Scripts/Menus/Initializer.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Initializer : MonoBehaviour {
-    void Start() {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenuScene");
+    void Awake() {
+        DontDestroyOnLoad(this);
+        StartCoroutine(delayStart());
+    }
+
+
+    IEnumerator delayStart() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Test Intro Scene");
+        yield return new WaitForSeconds(10f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
+        Destroy(this);
     }
 }
