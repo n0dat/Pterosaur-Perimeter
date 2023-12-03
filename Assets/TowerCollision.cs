b69851b42dbffd32322e9b578fd55870fcb415c0
James Cooper <74224020+n0dat@users.noreply.github.com>
2023-12-02 18:54:41 -0700

    Clean up tower placement
    Improves on #19

--- Assets/TowerCollision.cs
+++ Assets/TowerCollision.cs
@@ -1,12 +1,7 @@
 using UnityEngine;
 
 public class TowerCollision : MonoBehaviour {
-	public GameObject towerObj;
 	public Tower tower;
-
-	void Awake() {
-		tower = towerObj.GetComponent<Tower>();
-	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (!tower.getIsHeld())
